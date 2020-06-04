e2b75bda5124f38edffe4a05fab2907e7f5a2c79
Nick Carchedi <nick.carchedi@gmail.com>
2020-06-03 18:51:48 -0600

    get action overlay looking nice

--- src/ListedNote.js
+++ src/ListedNote.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Chip from "@material-ui/core/Chip";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    "& :hover": {
+      cursor: "pointer",
+    },
+  },
   tag: {
     margin: theme.spacing(0.25),
   },
@@ -15,14 +24,58 @@ const useStyles = makeStyles((theme) => ({
       textAlign: "right",
     },
   },
+  overlay: {
+    display: "flex",
+    position: "absolute",
+    zIndex: theme.zIndex.drawer + 1,
+    opacity: 0.8,
+    backgroundColor: theme.palette.grey[200],
+    borderRadius: theme.shape.borderRadius,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
 }));
 
 export default function ListedNote(props) {
-  const { note, tags, onRemoveTag } = props;
   const classes = useStyles();
+  const { note, tags, onRemoveTag } = props;
+  const [isHovered, setIsHovered] = useState(false);
+
+  const renderOverlay = () => {
+    return (
+      <Box
+        className={classes.overlay}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <IconButton
+          classes={classes.hoverButton}
+          aria-label="edit"
+          onClick={() => console.log("edit note: " + note.text)}
+        >
+          <EditIcon />
+        </IconButton>
+        <IconButton
+          classes={classes.hoverButton}
+          aria-label="delete"
+          onClick={() => console.log("delete note: " + note.text)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+    );
+  };
 
   return (
-    <ListItem key={note.id} divider onClick={() => console.log(note.id)}>
+    <ListItem
+      key={note.id}
+      className={classes.container}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && renderOverlay()}
       <Grid container alignItems="center">
         <Grid item xs={12} sm={6}>
           <ListItemText
